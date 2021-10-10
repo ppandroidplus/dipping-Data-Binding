5af764c9f5b80af02d6e104cd12673997d7d1787
Jinsu Park <parkjinsu4755@gmail.com>
2021-10-11 03:29:01 +0900

    circle ci: 실패하는 예제 작성

--- app/src/main/java/com/antilog/practice/databinding/MainActivity.kt
+++ app/src/main/java/com/antilog/practice/databinding/MainActivity.kt
@@ -3,8 +3,12 @@ package com.antilog.practice.databinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(
+            true
+            )
+        {1} else {2}
         super.onCreate(savedInstanceState)
     }
 }
