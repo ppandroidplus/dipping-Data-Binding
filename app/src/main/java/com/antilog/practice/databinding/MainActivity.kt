2ea223549d8cc560c06f90d275e40b287fdf454a
Jinsu Park <parkjinsu4755@gmail.com>
2021-10-11 03:37:52 +0900

    Update MainActivity.kt

--- app/src/main/java/com/antilog/practice/databinding/MainActivity.kt
+++ app/src/main/java/com/antilog/practice/databinding/MainActivity.kt
@@ -3,7 +3,7 @@ package com.antilog.practice.databinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity:AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
